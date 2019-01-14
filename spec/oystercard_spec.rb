0448b1a398f32e46eb96bcaae03b3c5aec32f81a
Brooke Woolley <brooke.woolley@gmail.com>
2019-01-14 16:39:52 +0000

    Refactored balance to use attr_reader

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,7 +3,7 @@ require 'oystercard'
 describe Oystercard do
 
   it 'returns the current balance' do
-    expect(subject.balance).to eq (0)
+    expect(subject.instance_variable_get(:@balance)).to eq (0)
   end
 
 end
