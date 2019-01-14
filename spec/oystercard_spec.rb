d2377db0b245c0794edf19c26cc5ed0fc31ab31f
Brooke Woolley <brooke.woolley@gmail.com>
2019-01-14 17:22:40 +0000

    Added functionality to top-up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,7 +8,14 @@ describe Oystercard do
 
   it 'can be topped up' do
     expect(subject).to respond_to(:top_up).with(1).argument
+  end
 
+  it 'returns the updated balance after top up' do
+    card = Oystercard.new
+    card.top_up(5)
+    expect(card.balance).to eq (5)
+    # mocked_balance = double(subject.balance)
+    # allow(mocked_balance).to receive(subject.top_up(5)).and_return(5)
   end
 
 end
