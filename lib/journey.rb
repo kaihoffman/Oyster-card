d68224d632c874b782f32eef4f106bd7207d9aed
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-01-17 19:35:17 +0000

    Implement penalty fares and minimum fare in Journey

--- lib/journey.rb
+++ lib/journey.rb
@@ -3,10 +3,30 @@ class Journey
   attr_reader :entry_station
   attr_accessor :exit_station
 
+  PENALTY_FARE = 6
+  MINIMUM_FARE = 1
+
   def initialize(args)
     empty_station = ''
     @entry_station = args[:in] || empty_station
     @exit_station = args[:out] || empty_station
+  end
+
+  def calculate_fare
+    penalty = missed_entry_station? || missed_exit_station?
+
+    penalty ? PENALTY_FARE : MINIMUM_FARE
+
+  end
+
+  private
+
+  def missed_entry_station?
+    entry_station == '' && exit_station != ''
+  end
+
+  def missed_exit_station?
+    exit_station == '' && entry_station != ''
   end
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -14,11 +14,20 @@ describe Journey do
 
   context 'it should charge penalties when missing taps' do
 
-    it 'levies a penalty fare when no entry station present' do
+    it 'levies a penalty fare when user forgets to touch out' do
       no_entry_journey = Journey.new(out: 'Aldgate')
-      expect(no_entry_journey.calculate_fare.to eq(Journey::PENALTY_FARE))
+      expect(no_entry_journey.calculate_fare).to eq(Journey::PENALTY_FARE)
     end
 
+    it 'levies a penalty fare when user forgets to touch in' do
+      no_exit_journey = Journey.new(in: 'Paddington')
+      expect(no_exit_journey.calculate_fare).to eq(Journey::PENALTY_FARE)
+    end
+
+  end
+
+  it 'should charge the minimum fare if no penalty' do
+    expect(journey.calculate_fare).to eq(Journey::MINIMUM_FARE)
   end
 
 
