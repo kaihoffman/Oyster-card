7ecbc25d9c160b1123df43048e26508fb40a54eb
Kai Hoffman <notkaiho@gmail.com>
2019-01-17 14:21:21 +0000

    refactored hash generation

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -23,13 +23,22 @@ class Oystercard
 
   def touch_in(entry_station)
     fail "Insufficient funds" if insufficient_funds?
+    # @journeys.store(entry_station, nil)
     @entry_station = entry_station
+    @journeys << { @entry_station => nil }
     end
 
   def touch_out(exit_station)
     deduct(MIN_JOURNEY_VALUE)
-    @exit_station = exit_station
-    store_journeys
+    @journeys.each do |hash|
+      if hash.key?(@entry_station)
+        hash[@entry_station] = exit_station
+      end
+    end
+    # @journeys << { self.keys.last => exit_station }
+    # @journeys.store(@journeys.keys.last, exit_station)
+    #@exit_station = exit_station
+    #store_journeys
   end
 
 private
@@ -45,8 +54,11 @@ private
     @balance -= amount
   end
 
-  def store_journeys
-    @journeys << { @entry_station => @exit_station }
-    @entry_station = nil
-  end
+  # def store_journeys
+  #   @journeys << { @entry_station => @exit_station }
+  #   @entry_station = nil
+  # end
 end
+
+
+# card.journeys[0][card.journeys[0].keys] = "firstexitstation"
