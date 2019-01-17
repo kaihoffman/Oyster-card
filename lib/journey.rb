5e11b527a9bee4138751edc4356ff9b20afc3dbb
Kai Hoffman <notkaiho@gmail.com>
2019-01-17 16:54:08 +0000

    Journey class receives hash of entry and exit station

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,9 +1,10 @@
 class Journey
   attr_reader :entry_station, :exit_station
 
-def initialize(entry_station, exit_station)
-  @entry_station = entry_station
-  @exit_station = entry_station
+def initialize(args)
+  empty_station = ""
+  @entry_station = args[:in] || empty_station
+  @exit_station = args[:out] || empty_station
 end
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,10 +1,15 @@
 require 'journey'
 describe Journey do
 
-subject(:journey) { described_class.new("Paddington", "Aldgate") }
+subject(:journey) { described_class.new({ in: "Paddington", out: "Aldgate" }) }
 
 it 'knows and can report start station for the executed instance' do
   expect(journey.entry_station).to eq("Paddington")
 end
+
+it 'knows and can report end station for the executed instance' do
+  expect(journey.exit_station).to eq("Aldgate")
+end
+
 
 end
