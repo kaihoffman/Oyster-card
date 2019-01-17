f9cfb3f200aa978d58b492ff57a01450985adf78
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-01-17 19:16:27 +0000

    Test penalty fare when missing touch-in

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -12,4 +12,14 @@ describe Journey do
     expect(journey.exit_station).to eq('Aldgate')
   end
 
+  context 'it should charge penalties when missing taps' do
+
+    it 'levies a penalty fare when no entry station present' do
+      no_entry_journey = Journey.new(out: 'Aldgate')
+      expect(no_entry_journey.calculate_fare.to eq(Journey::PENALTY_FARE))
+    end
+
+  end
+
+
 end
